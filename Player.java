956c37774cc4015169f8ec2e11278281408c2c56
Per Nyberg <per_skuru@msn.com>
2017-04-11 12:03:19 +0200

    Added comments for Player.java

--- Player.java
+++ Player.java
@@ -1,8 +1,11 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
+/**
+ * The player for the game. Very basic.
+ */
 public class Player {
-	public static int size = 20;
+	public static int size = 20; // same size for all players
 
 	private int x, y;
 	private boolean up;
@@ -14,32 +17,59 @@ public class Player {
 		up = false;
 	}
 
+	/**
+	 * Move the player with given values
+	 *
+	 * @param addX 	the change in x-coordinate
+	 * @param addY 	the change in y-coordinate
+	 */
 	public void move(int addX, int addY) {
 		x += addX;
 		y += addY;
 	}
 
+	/**
+	 * @param up 	whether or not the player moves up
+	 */
 	public void moveUp(boolean up) {
 		this.up = up;
 	}
 
+	/**
+	 * Set a new position for the player
+	 *
+	 * @param x 	the new x-coordinate
+	 * @param y 	the new y-coordinate
+	 */
 	public void setPosition(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * @return 	x-coordinate for the player
+	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * @return 	y-coordinate for the player
+	 */
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * @return 	if the player is moving up
+	 */
 	public boolean getUpMovement() {
 		return up;
 	}
 
+	/**
+	 * Paint the player
+	 */
 	public void paint(Graphics g) {
 		g.setColor(Color.red);
 		g.fillRect(x, y, size, size);
